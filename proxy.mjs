56fba3728a209e2c66ab2035dfa789a36b18fdc0
Nelson <nelson1967@gmail.com>
2023-08-25 19:11:38 -0400

    Mejorando el proxu de a poco

--- proxy.mjs
+++ proxy.mjs
@@ -1,12 +1,12 @@
 import * as net from 'node:net'
 const PORTENTRADA = process.env.PORTENTRADA ?? 1234
 const PORTDESTINO = process.env.PORTDESTINO ?? 4004
-const SRVDESTINO = process.env.SRVDESTINO ?? 'www.dcv.cl'
+const SRVDESTINO = process.env.SRVDESTINO ?? 'localhost'
 const server = net.createServer()
 
 server.listen(PORTENTRADA, () => {
   // const { port } = server.address()
-  console.log(`Proxy esperando conexión en localhost:${PORTENTRADA} y resuelve en destino localhost:${PORTDESTINO}`)
+  console.log(`Proxy esperando conexión en localhost:${PORTENTRADA} y resolviendo peticiones hacia destino ${SRVDESTINO}:${PORTDESTINO}`)
 })
 server.on('error', (err) => {
   console.log('Error:', err.name, err.message)
@@ -18,10 +18,13 @@ server.on('connection', (socket) => {
     const finLinea = d.indexOf(Buffer.from('\n'))
     const requerimiento = d.subarray(0, finLinea).toString()
     console.log('Requerimiento de entrada al Proxy recibida')
-    // console.log('------------------------------------------\n', requerimiento)
-    console.log('------------------------------------------\n', d.toString())
-    if (requerimiento.includes('POST')) {
-      console.log('Es un POST que va al servidore final (full request):\n', d.toString())
+    console.log('------------------------------------------\n', requerimiento)
+    // console.log('------------------------------------------\n', d.toString())
+    if (requerimiento.includes('POST') || requerimiento.includes('GET')) {
+      console.log('Es un POST o un GET que va al servidore final (full request):\n', d.toString())
+      if (requerimiento.includes('correo=')) {
+        requerimiento.replace('correo=', 'email=')
+      }
     }
     let muestraRespuesta = false
     if (d.includes('.js')) muestraRespuesta = true
